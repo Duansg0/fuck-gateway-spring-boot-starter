298a7774b7ec7b6b0b6e9d8960efdb59a82711c8
Duansg <duan_siguo@163.com>
2020-11-20 11:46:30 +0800

    //init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fuck-gateway-spring-boot-starter
+
+### 手撸高性能网关
---
+++ src/main/java/com/fuck/gateway/FuckNettyApplication.java
@@ -0,0 +1,14 @@
+package com.fuck.gateway;
+
+/**
+ * Application
+ * @author Duansg
+ * @version 1.0
+ * @date 2020/11/20 上午11:25
+ */
+public class FuckNettyApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
